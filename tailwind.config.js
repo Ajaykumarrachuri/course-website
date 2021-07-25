d74ce193c4302e8f998db0b22851274be7170eed
Ajaykumarrachuri <ajaykumar.rachuri@gmail.com>
2021-07-25 22:16:03 +0530

    Update: basic configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'sm': '375px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1440px',
+    },
+    colors: {
+      primary: 'var(--color-primary)',
+    },
+    extend: {
+      fontFamily: {
+        'sans': ['Apercu']
+      }
+    },
   },
   variants: {
     extend: {},
