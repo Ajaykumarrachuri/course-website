84944892ea45778cd9865153f2f01f66fbb08509
AJAY RACHURI <ajaykumar.rachuri@gmail.com>
2021-08-01 13:22:12 +0530

    Update: basic configurations (#2)
    * Update: basic configurations
    
    * Updated screens res

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'xs': '375px',
+      'sm': '576px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1440px',
+    },
+    colors: {
+      primary: 'var(--color-primary)',
+    },
+    extend: {
+      fontFamily: {
+        'sans': ['Apercu']
+      }
+    },
   },
   variants: {
     extend: {},
